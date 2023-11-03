740731683cc9af3571789f4c2adc1ec885f76285
TanakaChipise <tanakill15@gmail.com>
2023-11-03 13:37:24 +0100

    Commit

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,11 +1,11 @@
 public class Arrays {
     public static void main(String[] args) {
-        int Num[] = new int[10];
-        for (int i=0; i<Num.length; i++) {
-            Num[i]=9-i;
+        int myArray[] = new int[10];
+        for (int i=0; i<myArray.length; i++) {
+            myArray[i]=9-i;
         }
 
-        for (int element : Num) {
+        for (int element : myArray) {
             System.out.println(element);
 
         }
